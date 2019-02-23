268031a9f280b323c80a1d78148c7c9729dcd105
clau <czumaran@icluster.cl>
2019-02-23 01:36:51 -0300

    inicial

---
+++ proyecto-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Usuario/>
+  </div>
+</template>
+
+<script>
+import Usuario from './components/Usuario.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Usuario
+  }
+}
+</script>
+
